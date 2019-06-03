1852f519d65a0d3ff7f7744e17fa342a30a74fb2
jejecrunch <gkswlgp456@gmail.com>
2019-06-03 23:18:55 +0900

    190603 form 중에서 index.jsp, edit1.jsp 실습

---
+++ form0/src/main/java/net/skhu/controller/Form1Controller.java
@@ -0,0 +1,32 @@
+package net.skhu.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class Form1Controller {
+
+    // 첫 페이지
+    @RequestMapping("/")
+    public String index(Model model) {
+        return "index";
+    }
+
+    // edit1
+    @RequestMapping(value="/form1/edit1", method=RequestMethod.GET)
+    public String edit1(Model model) {
+        return "edit1";
+    }
+
+    @RequestMapping(value="/form1/edit1", method=RequestMethod.POST)
+    public String edit1(Model model, @RequestParam("title") String title,
+                                     @RequestParam("color") int color) {
+        model.addAttribute("title", title);
+        model.addAttribute("color", color);
+        System.out.printf("제목: %s. 색: %d\n", title, color);
+        return "edit1";
+    }
+}
