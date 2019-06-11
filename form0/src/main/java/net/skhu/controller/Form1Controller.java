3447e2292ddccace5fa7fdd591d6b02ffada7804
jejecrunch <gkswlgp456@gmail.com>
2019-06-11 20:37:42 +0900

    190611 edit2, edit3 실습

--- form0/src/main/java/net/skhu/controller/Form1Controller.java
+++ form0/src/main/java/net/skhu/controller/Form1Controller.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import net.skhu.model.Data;
+
 @Controller
 public class Form1Controller {
 
@@ -29,4 +31,32 @@ public class Form1Controller {
         System.out.printf("제목: %s. 색: %d\n", title, color);
         return "edit1";
     }
+
+    // edit2
+    @RequestMapping(value="/form1/edit2", method=RequestMethod.GET)
+    public String edit2(Model model) {
+        model.addAttribute("data", new Data());
+        return "edit2";
+    }
+
+    @RequestMapping(value="/form1/edit2", method=RequestMethod.POST)
+    public String edit2(Model model, Data data) {
+        System.out.printf("제목: %s. 색: %d\n", data.getTitle(), data.getColor());
+        return "edit2";
+    }
+
+    // edit3
+    @RequestMapping(value="/form1/edit3", method=RequestMethod.GET)
+    public String edit3(Model model) {
+        model.addAttribute("data", new Data());
+        return "edit3";
+    }
+
+    @RequestMapping(value="/form1/edit3", method=RequestMethod.POST)
+    public String edit3(Model model, Data data) {
+        System.out.printf("제목: %s. 색: %d\n", data.getTitle(), data.getColor());
+        return "edit3";
+    }
+
 }
+
