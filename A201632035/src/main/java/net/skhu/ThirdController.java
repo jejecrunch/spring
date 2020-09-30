9e2e74543a4b9093e38240b8de5c7f1c67730f48
jejecrunch <gkswlgp456@gmail.com>
2020-09-30 17:50:12 +0900

    구현과제 2 test2.jsp 구현

--- A201632035/src/main/java/net/skhu/ThirdController.java
+++ A201632035/src/main/java/net/skhu/ThirdController.java
@@ -2,6 +2,8 @@ package net.skhu;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -16,4 +18,30 @@ public class ThirdController {
 		model.addAttribute("std", student);
 		return "third/test1";
 	}
+
+	@GetMapping("test2")
+	public String test2(Model model) {
+		model.addAttribute("student", new Student());
+		return "third/test2";
+	}
+
+	@PostMapping("test2")
+	public String test2(Model model, Student student) {
+		// TODO: 저장하는 코드를 구현해야 함.
+		model.addAttribute("message", "저장되었습니다");
+		return "third/test2";
+	}
+
+	/*@RequestMapping(value="test3", method=RequestMethod.GET)
+	public String test3(Model model) {
+		model.addAttribute("student", new Student());
+		return "third/test2";
+	}
+
+	@RequestMapping(value="test3", method=RequestMethod.POST)
+	public String test3(Model model, Student student) {
+		// TODO: 저장하는 코드를 구현해야 함.
+		model.addAttribute("message", "저장되었습니다");
+		return "third/test2";
+	}*/
 }
