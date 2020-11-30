089d1adcd8dd9db9e972ded76778183c3a8804f6
jejecrunch <gkswlgp456@gmail.com>
2020-11-30 14:35:36 +0900

    백엔드 2차 구현실습 2번 JPA single value

---
+++ E201632035/src/main/java/net/skhu/E201632035Application.java
@@ -0,0 +1,13 @@
+package net.skhu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class E201632035Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(E201632035Application.class, args);
+	}
+
+}
