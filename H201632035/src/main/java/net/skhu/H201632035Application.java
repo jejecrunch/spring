f38457f675118e68fcfc6e08feacceffcea8c82e
jejecrunch <gkswlgp456@gmail.com>
2020-12-18 02:42:18 +0900

    백엔드 3차 구현실습 cities로 crud 만들기

---
+++ H201632035/src/main/java/net/skhu/H201632035Application.java
@@ -0,0 +1,13 @@
+package net.skhu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class H201632035Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(H201632035Application.class, args);
+	}
+
+}
