58c3eb11e9cb325091172514c361c5214846f4a2
jejecrunch <gkswlgp456@gmail.com>
2021-02-16 13:26:57 +0900

    210216 에러처리와 로그파일 생성 구현 실습

---
+++ error2/src/main/java/net/skhu/Error2Application.java
@@ -0,0 +1,13 @@
+package net.skhu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Error2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Error2Application.class, args);
+	}
+
+}
